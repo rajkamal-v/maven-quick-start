0357f7841f7728823c041fa968d2c24f91c135e2
Rajkamal Veerasamy <rajkamal.veerasamy@gmail.com>
2019-01-05 12:20:42 +0530

    added lang3 dependency and updated Application.java countWords method

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,12 +2,18 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
@@ -23,5 +29,8 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Words Count: " +count);
+		
     }
 }
